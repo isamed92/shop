0530437c19a643736b9c556863d8586268edc540
Isaias Medina <isamed92@gmail.com>
2023-05-14 02:24:33 -0300

    get products by page implemented using dio

--- lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -1,6 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/config.dart';
 import 'package:teslo_shop/features/products/domain/domain.dart';
+import 'package:teslo_shop/features/products/infrastructure/infrastructure.dart';
 
 class ProductsDatasourceImpl extends ProductDatasource {
+  late final Dio dio;
+  final String accessToken;
+
+  ProductsDatasourceImpl({required this.accessToken})
+      : dio = Dio(BaseOptions(
+            baseUrl: Environment.apiUrl,
+            headers: {'Authorization': 'Bearer $accessToken'}));
+
   @override
   Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
     // TODO: implement createUpdateProduct
@@ -20,8 +31,17 @@ class ProductsDatasourceImpl extends ProductDatasource {
   }
 
   @override
-  Future<List<Product>> getProductsByPage({int limit = 10, int offset = 0}) {
-    // TODO: implement getProductsByPage
-    throw UnimplementedError();
+  Future<List<Product>> getProductsByPage(
+      {int limit = 10, int offset = 0}) async {
+    final response = await dio.get<List>('/products',
+        queryParameters: {'limit': limit, 'offset': offset});
+
+    final List<Product> products = [];
+
+    for (var product in response.data ?? []) {
+      products.add(ProductMapper.jsonToEntity(product));
+    }
+
+    return products;
   }
 }
---
+++ lib/features/products/infrastructure/infrastructure.dart
@@ -0,0 +1,3 @@
+export './datasources/products_datasource_impl.dart';
+export './mappers/product_mapper.dart';
+export './respositories/products_repository_impl.dart';
